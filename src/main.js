c6fec43ea159cecfb3ad75ecc7214096c98bf31b
lefasom <leosombra89@gmail.com>
2023-08-14 23:20:34 +0200

    configuro vercel

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-/* import specific icons */
 import {
     faArrowLeft,
     faPaperPlane,
